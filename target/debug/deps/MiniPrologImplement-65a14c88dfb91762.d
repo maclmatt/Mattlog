be7e461bc565e4ce173fd1554c4a2e09d4e39d19
maclmatt <mayaclmatthews@gmail.com>
2025-01-28 13:25:31 +0000

    fix tests

--- target/debug/deps/MiniPrologImplement-65a14c88dfb91762.d
+++ target/debug/deps/MiniPrologImplement-65a14c88dfb91762.d
@@ -1,5 +1,5 @@
-/Users/mayamatthews/Documents/AYR3UoE/DISS/MiniPrologImplement/target/debug/deps/libMiniPrologImplement-65a14c88dfb91762.rmeta: src/main.rs
+/Users/mayamatthews/Documents/AYR3UoE/DISS/Mini-Prolog-Implementation/target/debug/deps/libMiniPrologImplement-65a14c88dfb91762.rmeta: src/main.rs
 
-/Users/mayamatthews/Documents/AYR3UoE/DISS/MiniPrologImplement/target/debug/deps/MiniPrologImplement-65a14c88dfb91762.d: src/main.rs
+/Users/mayamatthews/Documents/AYR3UoE/DISS/Mini-Prolog-Implementation/target/debug/deps/MiniPrologImplement-65a14c88dfb91762.d: src/main.rs
 
 src/main.rs:
--- target/debug/deps/integration_test-0a4353c5e1b1c986.d
+++ target/debug/deps/integration_test-0a4353c5e1b1c986.d
@@ -1,5 +1,5 @@
-/Users/mayamatthews/Documents/AYR3UoE/DISS/MiniPrologImplement/target/debug/deps/libintegration_test-0a4353c5e1b1c986.rmeta: tests/integration_test.rs
+/Users/mayamatthews/Documents/AYR3UoE/DISS/Mini-Prolog-Implementation/target/debug/deps/libintegration_test-0a4353c5e1b1c986.rmeta: tests/integration_test.rs
 
-/Users/mayamatthews/Documents/AYR3UoE/DISS/MiniPrologImplement/target/debug/deps/integration_test-0a4353c5e1b1c986.d: tests/integration_test.rs
+/Users/mayamatthews/Documents/AYR3UoE/DISS/Mini-Prolog-Implementation/target/debug/deps/integration_test-0a4353c5e1b1c986.d: tests/integration_test.rs
 
 tests/integration_test.rs:
